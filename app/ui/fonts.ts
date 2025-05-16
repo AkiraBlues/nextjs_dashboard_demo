24d7e93c2b4a4f54ba3504ff3544016c5390239d
Arctic <AkiraAngel7@gmail.com>
2025-05-16 14:47:22 +0800

    disable font preload to pass vercel build

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,8 +2,10 @@ import { LXGW_WenKai_Mono_TC, Lusitana } from 'next/font/google';
 
 export const lxgw_wenkai_tc = LXGW_WenKai_Mono_TC({
   weight: '300',
+  preload: false
 });
 
 export const lusitana = Lusitana({
   weight: '700',
+  preload: false
 });
